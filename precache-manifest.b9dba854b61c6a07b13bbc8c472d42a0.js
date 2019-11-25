baff301ecc00f99fa5748d2b888b639d4399794b
DanilLinkins <danil.p.box@gmail.com>
2019-11-25 13:20:13 +0300

    Updates

---
+++ precache-manifest.b9dba854b61c6a07b13bbc8c472d42a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a773476625ccad8d53b4da10f7ee4b92",
+    "url": "/default-vk-app/index.html"
+  },
+  {
+    "revision": "746ea522c5e60c10966c",
+    "url": "/default-vk-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "fa1348e0fae5cb71d857",
+    "url": "/default-vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "746ea522c5e60c10966c",
+    "url": "/default-vk-app/static/js/2.3ba07728.chunk.js"
+  },
+  {
+    "revision": "fa1348e0fae5cb71d857",
+    "url": "/default-vk-app/static/js/main.4d07f416.chunk.js"
+  },
+  {
+    "revision": "6b55793e728eabaa3236",
+    "url": "/default-vk-app/static/js/runtime-main.6ed6b959.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/default-vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
